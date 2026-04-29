60c3750fc6204bb1c2d11cb8269ea64c2632bb8d
Baji65 <baji.katari1@gmail.com>
2026-04-29 19:44:24 +0530

    29-04-2026

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -18,14 +18,29 @@ export const environment = {
 
 
 
-  authUrl: `${dev_url}/userDetails`,
-  sitesUrl: `${dev_url}/vipsites`,
-  metadataUrl: `${dev_url}/metadata`,
-  adsUrl: `${dev_url}/proximityAdsMain`,
-  rulesUrl: `${dev_url}/proximityAdsRules `,
-  sensorUrl:`${dev_url}/sensors`,
-  faqUrl: `${dev_url}/faq`,
-  inventoryUrl: `${dev_url}/inventory`,
-  genericUrl: `${dev_url}/supportRequests`,
-  eventsData: `${dev_url}/events_data`
+  // authUrl: `${dev_url}/userDetails`,
+  // sitesUrl: `${dev_url}/vipsites`,
+  // metadataUrl: `${dev_url}/metadata`,
+  // adsUrl: `${dev_url}/proximityAdsMain`,
+  // rulesUrl: `${dev_url}/proximityAdsRules `,
+  // sensorUrl:`${dev_url}/sensors`,
+  // faqUrl: `${dev_url}/faq`,
+  // inventoryUrl: `${dev_url}/inventory`,
+  // genericUrl: `${dev_url}/supportRequests`,
+  // eventsData: `${dev_url}/events_data`
+
+
+
+
+
+    authUrl: `${local_url}:3002/userDetails`,
+  sitesUrl: `${local_url}:3004/vipsites`,
+  metadataUrl: `${local_url}:3005/metadata`,
+  adsUrl: `${local_url}:8854/proximityAdsMain`,
+  rulesUrl: `${local_url}:8856/proximityAdsRules `,
+  sensorUrl:`${local_url}:8859/sensors`,
+  faqUrl: `${local_url}:8866/faq`,
+  inventoryUrl: `${local_url}:6465/inventory`,
+  genericUrl: `${local_url}:3003/supportRequests`,
+  eventsData: `${local_url}:3009/events_data`
 };
